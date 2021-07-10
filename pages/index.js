a7016c787353ea57d3fc25868a2f93073b18630c
Salman Mohammed <salm9nm@gmail.com>
2021-07-10 13:25:57 -0500

    changed class to className

--- pages/index.js
+++ pages/index.js
@@ -4,39 +4,39 @@ import Link from 'next/link'
 export default class MyComponent extends Component {
   render () {
     return (
-      <div class="container-fluid banner">
-      <div class="row">
-        <div class="col-md-12">
-      <nav class="navbar navbar-md">
-        <div class="navbar-brand">Salman Mohammed</div>
-        <ul class="nav nav-tabs">
-          <li class="nav-item">
+      <div className="container-fluid banner">
+      <div className="row">
+        <div className="col-md-12">
+      <nav className="navbar navbar-md">
+        <div className="navbar-brand">Salman Mohammed</div>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
                        <Link href="/">
-            <a class="nav-link">Home</a>
+            <a className="nav-link">Home</a>
                       </Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
                       <Link href="/posts/build-table">
-            <a class="nav-link">Country Table</a>
+            <a className="nav-link">Country Table</a>
                       </Link>
           </li>
-                      <li class="nav-item">
+                      <li className="nav-item">
           <Link href="/posts/country">
-            <a class="nav-link">Another Table</a>
+            <a className="nav-link">Another Table</a>
                        </Link>
                        </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">CONTACT</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">CONTACT</a>
           </li>
         </ul>
       </nav>
   </div>
-			<div class="col-md-8 offset-md-2 info">
-				<h1 class="text-center">Salmans Next JS Website</h1>
-				<p class="text-center">
+			<div className="col-md-8 offset-md-2 info">
+				<h1 className="text-center">Salmans Next JS Website</h1>
+				<p className="text-center">
 					Welcome to my website where I'm practicing my Next JS skills 
 				</p>
-				<a href="#" class="btn btn-md text-center">GET STARTED</a>
+				<a href="#" className="btn btn-md text-center">GET STARTED</a>
 			</div>
       </div>
       </div>
